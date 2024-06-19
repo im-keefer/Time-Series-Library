8fc3c87c2a55b9cfdd00ed0c02dbd5ae766ab0d1
NerduMiner <imkeefer@oakland.edu>
2024-06-19 11:06:37 -0400

    That was not a tensor

--- exp/exp_long_term_forecasting.py
+++ exp/exp_long_term_forecasting.py
@@ -225,8 +225,10 @@ class Exp_Long_Term_Forecast(Exp_Basic):
                         outputs = test_data.inverse_transform(outputs.squeeze(0)).reshape(shape)
                         batch_y = test_data.inverse_transform(batch_y.squeeze(0)).reshape(shape)
                     else: # We have multiple batches
-                        outputs = test_data.inverse_transform(outputs.unsqueeze(0).squeeze(0)).reshape(shape)
-                        batch_y = test_data.inverse_transform(batch_y.unsqueeze(0).squeeze(0)).reshape(shape)
+                        newshape = outputs.shape
+                        newshape[0] = 1
+                        outputs = test_data.inverse_transform(outputs.reshape(newshape).squeeze(0)).reshape(shape)
+                        batch_y = test_data.inverse_transform(batch_y.reshape(newshape).squeeze(0)).reshape(shape)
         
                 outputs = outputs[:, :, f_dim:]
                 batch_y = batch_y[:, :, f_dim:]
